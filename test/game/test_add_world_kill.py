0f872a1c874e78ce42a17c257c164079c89c19ad
Ana Luiza Pereira <analuper.alp@gmail.com>
2023-08-09 23:57:05 -0300

    test: add add_world_kill function test

--- test/game/test_add_world_kill.py
+++ test/game/test_add_world_kill.py
@@ -2,25 +2,24 @@ import unittest
 from domain.game import Game
 from service.game_service import create_filled_game
 
-class Add_player_kill_test(unittest.TestCase):
+class Add_world_kill_test(unittest.TestCase):
 
     def test_add_first_kill(self): 
         game = Game(1)
         game.add_player("Test")
-        game.add_player("Mocinha")
-        game.add_player_kill("Test")
-        self.assertEqual(game.players, {"Test", "Mocinha"})
-        self.assertEqual(game.kills, {"Test": 1, "Mocinha": 0})
+        game.add_world_kill("Test")
+        self.assertEqual(game.players, {"Test"})
+        self.assertEqual(game.kills, {"Test": -1})
     
     def test_add_new_kill(self): 
         game = create_filled_game(3, 3, {"Mocinha", "Dono da Bola"}, {"Dono da Bola": 2, "Mocinha": 1}, [])
-        game.add_player_kill("Mocinha")
+        game.add_world_kill("Mocinha")
         self.assertEqual(game.players, {"Mocinha", "Dono da Bola"})
-        self.assertEqual(game.kills, {"Dono da Bola": 2, "Mocinha": 2})
+        self.assertEqual(game.kills, {"Dono da Bola": 2, "Mocinha": 0})
 
     def test_add_invalid_player(self): 
         game = create_filled_game(3, 3, {"Mocinha", "Dono da Bola"}, {"Dono da Bola": 2, "Mocinha": 1}, [])
-        game.add_player_kill("Test")
+        game.add_world_kill("Test")
         self.assertEqual(game.players, {"Mocinha", "Dono da Bola"})
         self.assertEqual(game.kills, {"Dono da Bola": 2, "Mocinha": 1})
 
