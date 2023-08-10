e4037b0f792dd76f0bee3df4663941d46ade95c4
Ana Luiza Pereira <analuper.alp@gmail.com>
2023-08-10 13:13:42 -0300

    test: add game_controller get_game endpoint tests

---
+++ test/game_controller/test_get_name.py
@@ -0,0 +1,21 @@
+import unittest
+from unittest.mock import patch
+from werkzeug import exceptions
+from controller.game_controller import get_game
+
+class List_all_testl(unittest.TestCase):
+
+    def test_list_all_successful(self): 
+        result = get_game("test.log", 1)
+        expected_result = "{\"game_1\": {\"total_kills\": 0, \"players\": [\"Isgalamido\"], \"kills\": {\"Isgalamido\": 0}}}"
+        self.assertCountEqual(result, expected_result)
+
+    def test_list_all_unsuccessful(self):
+        with self.assertRaisesRegexp(exceptions.NotFound, "404 Not Found: File invalid.log not found. Try to use 'games.log' or 'test.log'"):
+            get_game("invalid.log", 1)
+        
+        with self.assertRaisesRegexp(exceptions.NotFound, "404 Not Found: Game number 11 not found"):
+            get_game("test.log", 11)        
+    
+if __name__ == "__main__":
+     unittest.main()
