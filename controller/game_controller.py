39a85774de6dbcfebbf831611b4cfde1f19ac49b
Ana Luiza Pereira <analuper.alp@gmail.com>
2023-08-10 11:26:48 -0300

    refactor: add file_name parameter in game_controller endpoints

--- controller/game_controller.py
+++ controller/game_controller.py
@@ -1,10 +1,10 @@
 from service.parser_service import file_reader
 import service.game_service as game_service
 
-def list_all():
-    games = file_reader("test.log")
+def list_all(file_name):
+    games = file_reader(file_name)
     return game_service.create_json_list(games)
 
-def get_game(game_number):
-    game = game_service.find_game(file_reader("test.log"), game_number)
+def get_game(file_name, game_number):
+    game = game_service.find_game(file_reader(file_name), game_number)
     return game_service.create_json(game)        
--- service/parser_service.py
+++ service/parser_service.py
@@ -1,15 +1,22 @@
 import re
+from flask import abort
 from domain.game import Game
 from enumeration.regex import Regex
 
 def file_reader(file):
-    log_file = open(file, "r+")
-    return parser(log_file)
+    try:
+        log_file = open(file, "r+")
+        return parser(log_file)
+    except FileNotFoundError:
+        abort(
+            404, f"File {file} not found. Try to use 'games.log' or 'test.log'"
+        )
 
 def parser(log_file):
     game_number = 0
     in_game = False
     games = []
+    game = Game(0)
 
     for row in log_file:
         if re.match(Regex.INIT_GAME.value, row):
@@ -24,12 +31,20 @@ def parser(log_file):
                 player = re.split("\\\\", row)
                 game.add_player(player[1]) 
             elif re.match(Regex.NEW_KILL.value, row):
-                killer = re.search(Regex.KILLER.value, row).group(1)
-                if re.fullmatch(killer, Regex.WORLD.value):
-                    killed = re.search(Regex.KILLED.value, row).group(1)
-                    game.add_world_kill(killed)
-                else: 
-                    game.add_player_kill(killer)
+                killer_re = re.search(Regex.KILLER.value, row)
+                if(killer_re): 
+                    killer = killer_re.group(1)
+                    if re.fullmatch(killer, Regex.WORLD.value):
+                        killed_re = re.search(Regex.KILLED.value, row)
+                        if(killed_re):
+                            killed = killed_re.group(1)
+                            game.add_world_kill(killed)
+                        else:
+                          game.alert_messages.append("Unable to find killed.")  
+                    else: 
+                        game.add_player_kill(killer)
+                else:
+                    game.alert_messages.append("Unable to find killer.")
             elif re.match(Regex.END_GAME.value, row):
                 games.append(game)
                 in_game = False
