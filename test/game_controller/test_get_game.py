356b6eb5fc5e692d7e77ad6d9b09ed9de6166ca6
Ana Luiza Pereira <analuper.alp@gmail.com>
2023-08-10 21:46:11 -0300

    refactor: change name of tests in test_get_game_file

--- test/game_controller/test_get_game.py
+++ test/game_controller/test_get_game.py
@@ -5,12 +5,12 @@ from controller.game_controller import get_game
 
 class Get_game_testl(unittest.TestCase):
 
-    def test_list_all_successful(self): 
+    def test_get_game_successful(self): 
         result = get_game("test.log", 1)
         expected_result = "{\"game_1\": {\"total_kills\": 0, \"players\": [\"Isgalamido\"], \"kills\": {\"Isgalamido\": 0}}}"
         self.assertCountEqual(result, expected_result)
 
-    def test_list_all_unsuccessful(self):
+    def test_get_game_unsuccessful(self):
         with self.assertRaisesRegex(exceptions.NotFound, "404 Not Found: File invalid.log not found. Try to use 'games.log' or 'test.log'"):
             get_game("invalid.log", 1)
         
